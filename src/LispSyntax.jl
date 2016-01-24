c804faa4032eb6c2daf8b303e4310202987fe61d
Ismael Venegas Castelló <ismael.vc1337@gmail.com>
2016-01-24 12:48:16 -0600

    Use multiple dispatch, avoid recomputing length, add type anotations.

--- src/LispSyntax.jl
+++ src/LispSyntax.jl
@@ -10,7 +10,7 @@ include("parser.jl")
 
 # Internal types
 type SExpr
-  vector
+  vector::Vector{Any}
 end
 
 sx(x...) = SExpr([x...])
@@ -19,61 +19,38 @@ sx(x...) = SExpr([x...])
 
 length(x::SExpr) = length(x.vector)
 
-function desx(s)
-  if typeof(s) == SExpr
-    return map(desx, s.vector)
-  elseif isa(s, Dict)
-    return Dict(map(x -> desx(x[1]) => desx(x[2]), s)...)
-  elseif isa(s, Set)
-    return Set(map(desx, s))
-  else
-    return s
-  end
-end
+desx(s) = s
+desx(s::SExpr) = map(desx, s.vector)
+desx(s::Dict) = Dict(map(x -> desx(x[1]) => desx(x[2]), s)...)
+desx(s::Set) = Set(map(desx, s))
 
-function lispify(s)
-  if isa(s, SExpr)
-    return "(" * join(map(lispify, s.vector), " ") * ")"
-  else
-    return "$s"
-  end
-end
+lispify(s) = string(s)
+lispify(s::SExpr) = string("(", join(map(lispify, s.vector), " "), ")")
 
-function construct_sexpr(items...) # convert the input tuple to an array
-  ret = Array(Any, length(items))
-  for i = 1:length(items)
-    ret[i] = items[i]
-  end
-  ret
-end
+"Convert the input tuple to an array."
+construct_sexpr(items...) = collect(items)
 
 function assign_reader_dispatch(sym, fn)
   reader_table[sym] = fn
 end
 
-function quasiquote(s, escape_exceptions)
-  if isa(s, Array) && length(s) == 2 && s[1] == :splice
-    codegen(s[2], escape_exceptions = escape_exceptions)
-  elseif isa(s, Array) && length(s) == 2 && s[1] == :splice_seq
-    Expr(:..., codegen(s[2], escape_exceptions = escape_exceptions))
-  elseif isa(s, Array)
-    Expr(:call, :construct_sexpr, map(s -> quasiquote(s, escape_exceptions), s)...)
-  elseif isa(s, Symbol)
-    Expr(:quote, s)
-  else
-    s
-  end
+quasiquote(s, escape_exceptions::Set{Symbol}) = s
+quasiquote(s::Symbol, escape_exceptions::Set{Symbol}) = Expr(:quote, s)
+
+function quasiquote(s::Vector{Any}, escape_exceptions::Set{Symbol})
+    len_s = length(s)
+    if len_s == 2 && s[1] == :splice
+        codegen(s[2], escape_exceptions = escape_exceptions)
+    elseif len_s == 2 && s[1] == :splice_seq
+        Expr(:..., codegen(s[2], escape_exceptions = escape_exceptions))
+    else
+        Expr(:call, :construct_sexpr, map(s -> quasiquote(s, escape_exceptions), s)...)
+    end
 end
 
-function quote_it(s)
-  if isa(s, Array)
-    Expr(:call, :construct_sexpr, map(s -> quote_it(s), s)...)
-  elseif isa(s, Symbol)
-   QuoteNode(s)
-  else
-    s
-  end
-end
+quote_it(s) = s
+quote_it(s::Symbol) = QuoteNode(s)
+quote_it(s::Vector{Any}) = Expr(:call, :construct_sexpr, map(s -> quote_it(s), s)...)
 
 function codegen(s; escape_exceptions = Set{Symbol}())
   if isa(s, Symbol)
@@ -92,12 +69,13 @@ function codegen(s; escape_exceptions = Set{Symbol}())
     Expr(:call, :Set, Expr(:vect, coded_s...))
   elseif !isa(s, Array) # constant
     s
-  elseif length(s) == 0 # empty array
+  elseif isempty(s) # empty array
     s
   elseif s[1] == :if
-    if length(s) == 3
+    len_s = length(s)
+    if len_s == 3
       :($(codegen(s[2], escape_exceptions = escape_exceptions)) && $(codegen(s[3], escape_exceptions = escape_exceptions)))
-    elseif length(s) == 4
+    elseif len_s == 4
       :($(codegen(s[2], escape_exceptions = escape_exceptions)) ? $(codegen(s[3], escape_exceptions = escape_exceptions)) : $(codegen(s[4],  escape_exceptions = escape_exceptions)))
     else
       error("illegal if statement $s")
@@ -106,16 +84,18 @@ function codegen(s; escape_exceptions = Set{Symbol}())
     assert(length(s) == 3)
     :($(esc(s[2])) = $(codegen(s[3], escape_exceptions = escape_exceptions)))
   elseif s[1] == :let
-    syms     = Set([ s[2][i] for i = 1:2:length(s[2]) ])
-    bindings = [ :($(s[2][i]) = $(codegen(s[2][i+1], escape_exceptions = escape_exceptions ∪ syms))) for i = 1:2:length(s[2]) ]
+    len_s2 = length(s[2])
+    syms     = Set{Symbol}([ s[2][i] for i = 1:2:len_s2 ])
+    bindings = [ :($(s[2][i]) = $(codegen(s[2][i+1], escape_exceptions = escape_exceptions ∪ syms))) for i = 1:2:len_s2 ]
     coded_s  = map(x -> codegen(x, escape_exceptions = escape_exceptions ∪ syms), s[3:end])
     Expr(:let, Expr(:block, coded_s...), bindings...)
   elseif s[1] == :while
     coded_s = map(x -> codegen(x, escape_exceptions = escape_exceptions), s[2:end])
     Expr(:while, coded_s[1], Expr(:block, coded_s[2:end]...))
   elseif s[1] == :for
-    syms     = Set([ s[2][i] for i = 1:2:length(s[2]) ])
-    bindings = [ :($(s[2][i]) = $(codegen(s[2][i+1], escape_exceptions = escape_exceptions ∪ syms))) for i = 1:2:length(s[2]) ]
+    len_s2 = length(s[2])
+    syms     = Set{Symbol}([ s[2][i] for i = 1:2:len_s2 ])
+    bindings = [ :($(s[2][i]) = $(codegen(s[2][i+1], escape_exceptions = escape_exceptions ∪ syms))) for i = 1:2:len_s2 ]
     coded_s  = map(x -> codegen(x, escape_exceptions = escape_exceptions ∪ syms), s[3:end])
     Expr(:for, Expr(:block, bindings...), Expr(:block, coded_s...))
   elseif s[1] == :do
@@ -134,19 +114,19 @@ function codegen(s; escape_exceptions = Set{Symbol}())
     quasiquote(s[2], escape_exceptions)
   elseif s[1] == :lambda || s[1] == :fn
     assert(length(s) >= 3)
-    coded_s = map(x -> codegen(x, escape_exceptions = escape_exceptions ∪ Set(s[2])), s[3:end])
+    coded_s = map(x -> codegen(x, escape_exceptions = escape_exceptions ∪ Set{Symbol}(s[2])), s[3:end])
     Expr(:function, Expr(:tuple, s[2]...), Expr(:block, coded_s...))
   elseif s[1] == :defn
     # Note: julia's lambdas are not optimized yet, so we don't define defn as a macro.
     #       this should be revisited later.
-    coded_s = map(x -> codegen(x, escape_exceptions = escape_exceptions ∪ Set(s[3])), s[4:end])
+    coded_s = map(x -> codegen(x, escape_exceptions = escape_exceptions ∪ Set{Symbol}(s[3])), s[4:end])
     Expr(:function, Expr(:call, esc(s[2]), s[3]...), Expr(:block, coded_s...))
   elseif s[1] == :defmacro
      Expr(:macro, Expr(:call, esc(s[2]), s[3]...),
           begin
-            coded_s = map(x -> codegen(x, escape_exceptions = escape_exceptions ∪ Set(s[3])), s[4:end])
+            coded_s = map(x -> codegen(x, escape_exceptions = escape_exceptions ∪ Set{Symbol}(s[3])), s[4:end])
             sexpr = Expr(:block, coded_s...) #codegen(s[4], escape_exceptions = escape_exceptions ∪ Set(s[3]))
-            :(codegen($sexpr, escape_exceptions = $escape_exceptions ∪ Set($(s[3]))))
+            :(codegen($sexpr, escape_exceptions = $escape_exceptions ∪ Set{Symbol}($(s[3]))))
           end)
   elseif s[1] == :defmethod
     # TODO
